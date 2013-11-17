62d4ec0f8fb23605700b21d492a3d6d9d1fd3317
all4senses <all4senses@gmail.com>
2013-11-17 21:22:32 +0700

    Deny add to cart

--- modules/_ZP/zp_commerce/js/zp_deny_addtocart_message.js
+++ modules/_ZP/zp_commerce/js/zp_deny_addtocart_message.js
@@ -3,13 +3,14 @@
   Drupal.behaviors.zp_deny_addtocart_message = {
     attach: function (context, settings) {
        
-       console.log('test');
-       console.log(Drupal.settings['zp_commerce']);
-       console.log(Drupal.settings['zp_commerce']['deny_message']);
+//       console.log('test');
+//       console.log(Drupal.settings['zp_commerce']);
+//       console.log(Drupal.settings['zp_commerce']['deny_message']);
        
        $(".deny-addtocart a").click(function(){
          
-         console.log('click addtocart: ' + Drupal.settings['zp_commerce']['deny_message']);
+         //console.log('click addtocart: ' + Drupal.settings['zp_commerce']['deny_message']);
+         alert(Drupal.settings['zp_commerce']['deny_message']);
          return false;
 
        });
