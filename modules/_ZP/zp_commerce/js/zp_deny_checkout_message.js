9e392f6e3dee143512dbcc1e649fee1d08511902
all4senses <all4senses@gmail.com>
2013-11-22 06:23:02 +0700

    Deny checkout

--- modules/_ZP/zp_commerce/js/zp_deny_checkout_message.js
+++ modules/_ZP/zp_commerce/js/zp_deny_checkout_message.js
@@ -1,16 +1,16 @@
 (function ($) {
 
-  Drupal.behaviors.zp_deny_addtocart_message = {
+  Drupal.behaviors.zp_deny_checkout_message = {
     attach: function (context, settings) {
        
 //       console.log('test');
 //       console.log(Drupal.settings['zp_commerce']);
 //       console.log(Drupal.settings['zp_commerce']['deny_message']);
        
-       $(".deny-addtocart a").click(function(){
+       $(".deny-checkout a").click(function(){
          
          //console.log('click addtocart: ' + Drupal.settings['zp_commerce']['deny_message']);
-         alert(Drupal.settings['zp_commerce']['deny_addtocart_message']);
+         alert(Drupal.settings['zp_commerce']['deny_checkout_message']);
          return false;
 
        });
