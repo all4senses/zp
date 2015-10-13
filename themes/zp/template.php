4c70d07f6ade4850116ac781abd2c451e84dd917
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-10-13 21:18:35 +0600

    ZP import

--- themes/zp/template.php
+++ themes/zp/template.php
@@ -590,6 +590,8 @@ function zp_breadcrumb($variables) {
 
   if (!empty($breadcrumb)) {
     
+    dpm($breadcrumb);
+    
     // Adding the title of the current page to the breadcrumb.
     $breadcrumb[] = drupal_get_title();
     
