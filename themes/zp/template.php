66633a9bba54e4d46f52029a80487ce2024941ec
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-10-17 03:23:17 +0600

    ZP catalog structure

--- themes/zp/template.php
+++ themes/zp/template.php
@@ -570,12 +570,12 @@ function zp_menu_local_tasks(&$variables) {
 }
 
 
-
+/*
 function zp_preprocess_breadcrumb(&$variables) {
   dpm($variables);
   dpm(drupal_get_breadcrumb());
 }
-
+*/
 
 
 /**
@@ -587,7 +587,7 @@ function zp_preprocess_breadcrumb(&$variables) {
  */
 function zp_breadcrumb($variables) {
   
-  dpm($variables);
+  //dpm($variables);
   
   return zp_misc_setBreadcrumbs($variables);
   
