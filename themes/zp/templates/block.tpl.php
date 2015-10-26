375f9a0e539ed291e646cfbb2b235e5873044410
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-10-27 02:57:23 +0600

    ZP Import

--- themes/zp/templates/block.tpl.php
+++ themes/zp/templates/block.tpl.php
@@ -11,7 +11,7 @@ if (!$content) {
 <?php endif; ?>
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
+  <?php if (@$title): ?>
     <?php echo ($block->region == 'content' ? '<h2 ' : '<h3 ') . $title_attributes; ?>><?php echo $title . ($block->region == 'content' ? '</h2>' : '</h3>'); ?>
   <?php endif;?>
   <?php print render($title_suffix); ?>
