177011f06248e8bb3d6cd7e9c84b57c3d8948d71
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-10-27 02:31:29 +0600

    ZP Import

--- themes/zp/block--bare.tpl.php
+++ themes/zp/block--bare.tpl.php
@@ -7,7 +7,7 @@ if (!$content) {
 //dpm($content_attributes);
 ?>
 <?php print render($title_prefix); ?>
-<?php if ($title): ?>
+<?php if (@$title): ?>
   <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
 <?php endif; ?>
 <?php print render($title_suffix); ?>
