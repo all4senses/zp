4e4e5a82270a9a8a703be380e041d9419c277e5b
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-11-01 05:16:36 +0600

    ZP articles

--- modules/_ZP/zp_commerce/js/zp_commerce_setFacetArticles.js
+++ modules/_ZP/zp_commerce/js/zp_commerce_setFacetArticles.js
@@ -8,6 +8,7 @@
       // http://stackoverflow.com/questions/2655925/how-to-apply-important-using-css
       // http://stackoverflow.com/questions/11962962/overriding-important-with-css-or-jquery
       
+      // Make facets checkboxes float left to see it normally
       jQuery('.block-facetapi input').each(function () {
         //Works
         //this.style.setProperty("float", "left", "important");
@@ -17,6 +18,7 @@
         //$(this).addClass('xxx');
       });
       
+      // Initially close all expanded categories
       $('.block-facetapi .facetapi-facet-field-article-product-depts .item-list .item-list').hide();
       
 //      $('.block-facetapi .facetapi-facet-field-article-product-depts li').each(function(){
@@ -25,7 +27,7 @@
 //          $(this).find('.item-list').hide();
 //        }
 //      });
-      
+      // And then open those branches that has active links (from the upper parent down to a first child)
       $('.block-facetapi .facetapi-facet-field-article-product-depts .item-list:not(zp-processed) li a.facetapi-active').each(function(){
         $(this).parent().find('.item-list').show();
         $(this).parent().children('.item-list').show().children('.item-list').hide();
