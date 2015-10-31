caa3f6e0eed10fbd9f3e052b82d9fc3a7f54bfd5
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-10-31 22:20:29 +0600

    ZP articles

--- modules/_ZP/zp_commerce/js/zp_commerce_setFacetArticles.js
+++ modules/_ZP/zp_commerce/js/zp_commerce_setFacetArticles.js
@@ -7,10 +7,23 @@
       
       //var cat_name = null;
       
+      jQuery('.block-facetapi .facetapi-facet-field-article-product-depts li').each(function(){
+        found = null;
+        found = $(this).find('a.facetapi-active');
+        console.log(found);
+        if (found.length){
+          console.log('found = 1');
+        }
+        else {
+          console.log('found = 0');
+        }
+        
+      });
+      
       jQuery('.block-facetapi .facetapi-facet-field-article-product-depts li a.facetapi-inactive').each(function(){
         
-        console.log(this);
-        $(this).parent().find('.item-list').hide();
+        //console.log(this);
+        //$(this).parent().find('.item-list').hide();
 
 //        cat_name = jQuery(this).find('.cat-name');
 //        
