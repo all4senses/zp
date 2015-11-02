c2e81a6fe8c9454cc416dcc766b8d3d019c347de
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-11-02 23:05:03 +0600

    ZP articles

--- modules/_ZP/zp_commerce/js/zp_commerce_setFacetArticles.js
+++ modules/_ZP/zp_commerce/js/zp_commerce_setFacetArticles.js
@@ -55,6 +55,12 @@
       
       $('.zp-expand').click(function(){
         console.log('click');
+        if ($(this).hasClass(opened)) {
+          $(this).html('<span class="zp-expand closed" style="float: left">+</span>');
+        }
+        else {
+          $(this).html('<span class="zp-expand opened" style="float: left">-</span>');
+        }
       });
       
     }
