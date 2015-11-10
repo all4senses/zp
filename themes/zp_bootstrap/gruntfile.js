87959bae4c0e8f1cab6a6265b58446f2db523f35
U-FLYSMALL\Alex <all4senses@gmail.com>
2015-11-10 21:24:12 +0600

    ZP theming

--- themes/zp_bootstrap/gruntfile.js
+++ themes/zp_bootstrap/gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
       //grunt.loadNpmTasks('grunt-autoprefixer'); // Deprecated
       grunt.loadNpmTasks('grunt-postcss');
       grunt.loadNpmTasks('grunt-contrib-uglify');
-      grunt.loadNpmTasks('grunt-w3c-markup-validation');
+      //grunt.loadNpmTasks('grunt-w3c-markup-validation'); // Doesn't work... :( Warning: Cannot read property 'length' of undefined Use --force to continue.
       
       grunt.initConfig({
           uglify: {
@@ -95,50 +95,95 @@ module.exports = function (grunt) {
           },
           */
          
+         
           postcss: {
-              options: {
-                //map: true, // inline sourcemaps
+              prod: {
+                  options: {
+                      //map: true, // inline sourcemaps
 
-                // or
-                /*
-                map: {
-                    inline: false, // save all sourcemaps as separate files...
-                    annotation: 'dist/css/maps/' // ...to the specified directory
-                },
-                */
-                processors: [
-                  require('pixrem')(), // add fallbacks for rem units
-                  require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
-                  require('cssnano')() // minify the result
-                ]
-              },
-              
-              dist: {
-                // src: 'css/custom.css',
-                // dest: 'css/custom_postcss.css'
-                // Works.
-                files: {
-                    'css/postcss/custom_postcss.css': 'css/custom.css', // This will not be actually used, cause it's added to style.css (below)
-                    'css/postcss/style_postcss.css': 'css/style.css'
-                }
-              },
-              
-              // seems doesn't work
-              /*
-              multiple_files: [{
-                //expand: true,
-                //flatten: true,
-                src: 'css/*.css', // -> src/css/file1.css, src/css/file2.css
-                dest: 'css/postcss/' // -> dest/css/file1.css, dest/css/file2.css
-              }]
-              */
-            
-          },
+                      // or
+                      /*
+                      map: {
+                          inline: false, // save all sourcemaps as separate files...
+                          annotation: 'dist/css/maps/' // ...to the specified directory
+                      },
+                      */
+                      processors: [
+                          require('pixrem')(), // add fallbacks for rem units
+                          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                          require('cssnano')() // minify the result
+                      ] // processors
+                  }, // options
+                  
+                  dist: {
+                    // src: 'css/custom.css',
+                    // dest: 'css/custom_postcss.css'
+                    // Works.
+                    files: {
+                        'css/postcss/custom_postcss.css': 'css/custom.css', // This will not be actually used, cause it's added to style.css (below)
+                        'css/postcss/style_postcss.css': 'css/style.css'
+                    }
+                  },
+
+                  // seems doesn't work
+                  /*
+                  multiple_files: [{
+                    //expand: true,
+                    //flatten: true,
+                    src: 'css/*.css', // -> src/css/file1.css, src/css/file2.css
+                    dest: 'css/postcss/' // -> dest/css/file1.css, dest/css/file2.css
+                  }]
+                  */
+              }, // prod
+              dev: {
+                  options: {
+                      //map: true, // inline sourcemaps
+
+                      // or
+                      /*
+                      map: {
+                          inline: false, // save all sourcemaps as separate files...
+                          annotation: 'dist/css/maps/' // ...to the specified directory
+                      },
+                      */
+                      processors: [
+                          require('pixrem')(), // add fallbacks for rem units
+                          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                          //require('cssnano')() // minify the result
+                      ] // processors
+                  }, // options
+                  
+                  
+                  dist: {
+                    // src: 'css/custom.css',
+                    // dest: 'css/custom_postcss.css'
+                    // Works.
+                    files: {
+                        'css/postcss/custom_postcss.css': 'css/custom.css', // This will not be actually used, cause it's added to style.css (below)
+                        'css/postcss/style_postcss.css': 'css/style.css'
+                    }
+                  },
+
+                  // seems doesn't work
+                  /*
+                  multiple_files: [{
+                    //expand: true,
+                    //flatten: true,
+                    src: 'css/*.css', // -> src/css/file1.css, src/css/file2.css
+                    dest: 'css/postcss/' // -> dest/css/file1.css, dest/css/file2.css
+                  }]
+                  */
+              } // dev
+          }, // compass
+         
+          // Doesn't work... :( Warning: Cannot read property 'length' of undefined Use --force to continue.
+          /*
           w3c_markup_validation : {
               //failOnError : false,
               pages : ['templates/*.html'], //['templates/*.tpl.php'], //['./test-pages/fails.html', 'http://www.google.com'],
               ignore : ['no document type delaration']
           },
+          */
           watch: {
               sass: {
                   files: [
@@ -156,12 +201,16 @@ module.exports = function (grunt) {
               */
               postcss: {
                   files: ['css/*.css'], 
-                  tasks: ['postcss']
+                  tasks: ['postcss:dev']
               },
+              
+              // Doesn't work... :( Warning: Cannot read property 'length' of undefined Use --force to continue.
+              /*
               w3c_markup_validation: {
                   files: ['templates/*.html'], 
                   tasks: ['w3c_markup_validation']
               },
+              */
               javascripts: {
                   files: ['assets/js/*.js'], // We only need to watch the custom js
                   tasks: ['uglify:js_dev']
@@ -169,5 +218,5 @@ module.exports = function (grunt) {
           } // watch
       }); // initConfig
       grunt.registerTask('default', 'watch');
-      grunt.registerTask('prod', ['uglify:js_prod', 'compass:prod', 'postcss']);
+      grunt.registerTask('prod', ['uglify:js_prod', 'compass:prod', 'postcss:prod']);
 } // exports
